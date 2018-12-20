8ce7eebdde9367bb05075857d3cb0e7704abd412
mrliu <9325186@qq.com>
2018-12-21 00:43:14 +0800

    享信课堂程序，用于职场培训课程 thinkphp5。1 php 7.0版本

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\facade\Config;
+use think\Controller;
+
+class Index extends Controller
+{
+    //载入后台视图
+    public function index(){
+
+
+
+        return view();
+    }
+}
---
+++ application/admin/controller/Login.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Login extends Controller
+{
+    //登陆页面
+    public function login(){
+
+        return view();
+    }
+}
