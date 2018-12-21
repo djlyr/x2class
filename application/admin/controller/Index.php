b173a6f27f6e2ed043159e74189c19c347027a7b
mrliu <9325186@qq.com>
2018-12-22 04:37:51 +0800

    享信课堂程序，用于职场培训课程 thinkphp5。1 php 7.0版本

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use think\facade\Config;
 use think\Controller;
+use think\facade\Cache;
+use think\facade\Session;
 
 class Index extends Controller
 {
@@ -11,7 +13,20 @@ class Index extends Controller
     public function index(){
 
 
+        //判断登陆后session是否存入数据
+        if(session('admin_id')){
+            return view();
+        }else{
+            return $this->error('请登陆后，在进行操作！','admin/login/login');
+        }
+    }
 
-        return view();
+    public function cache(){
+        Cache::clear();
+        return $this->success('缓存清理完成！！','admin/index/index');
+    }
+    public function clear(){
+        Session::clear();
+        return $this->success('退出成功！！','admin/login/login');
     }
 }
--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -3,12 +3,50 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
+use think\Request;
+use think\Validate;
 
 class Login extends Controller
 {
+
     //登陆页面
     public function login(){
 
+
         return view();
+    }
+    //获取POST数据
+    public function post(Request $request)
+    {
+        $post =$request->post();    //获取POST数据
+        //halt($post);
+        //对POST数据验证，定义一个验证规则
+        $validate = Validate::make([
+            'admin_username|账号' => 'require|min:5|max:20',
+            'admin_password|密码' => 'require|min:8|max:32',   //验证是否必填,最小8位，最大32位
+            'code|验证码'  => 'require|captcha',
+        ]);
+        //用验证器对POST进行验证
+        $status = $validate -> check($post);
+        //可以通过判断真假来返回数据
+        if ($status){
+            //为真走进这里
+            $user = Db::table('xx_admin')->where('admin_username',$post['admin_username'])->where('admin_password',md5($post['admin_password']))->find();
+            if ($user){
+                //找到数据就执行这里
+                //如果账号密码正确，就把id和账号存入session
+                session('admin_id',$user['admin_id']);
+                session('admin_username',$user['admin_username']);
+                return $this->success('登陆成功！！','admin/index/index');
+            }else{
+                //找不到数据就走这里
+                return $this->error('账号或密码错误！！');
+            }
+        }else{
+            //为假走进这里
+            return $this->error($validate->getError());
+        }
+
     }
 }
