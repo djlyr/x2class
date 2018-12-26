c294cc3c552511fc43c64e09761ade0e22ad6166
mrliu <9325186@qq.com>
2018-12-27 03:58:28 +0800

    享信课堂程序，用于职场培训课程 thinkphp5。1 php 7.0版本

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -48,4 +48,72 @@ class Cate extends Controller
         }
 
     }
+    public function addid($id){
+        $cid = DB('cate')->where('id',$id)->find();
+        if(!$cid){
+            return $this->error('提交数据错误，请返回重新提交！');
+        }else{
+            return view()->assign('cid',$cid);
+        }
+
+    }
+
+    public function edit($id){
+
+        $did = Db('cate')->where('id',$id)->find();
+
+        if($did){
+            //能查询到数据
+
+            $qt['qid'] = Db('cate')->select();
+            $eid=array_merge($did,$qt);
+            if(!$eid){
+                return $this->error('提交数据错误，请返回重新提交！');
+            }else{
+                return view()->assign('eid',$eid);
+            }
+
+        }else{
+            //不能查询到数据
+            return $this->error('未找到编辑栏目，请重新提交查询！！！');
+        }
+
+    }
+
+    public function editpost(){
+        $post = Request()->ispost();
+        if($post){
+            $validate = Validate::make([
+                'catename|中文栏目名称' => 'require|chs',
+                'cate_ename|英文栏目名称' => 'require|alpha',
+            ]);
+            $post = input('post.');
+            $status = $validate -> check($post);
+            if($status){
+                $cpost = Db('cate')->where('id',$post['id'])->find();
+                if($cpost){
+                    Db('cate')->where('id',$post['id'])->update($post);
+                    return $this->success('更新栏目成功','lst');
+                }else{
+                    return $this->error('未找到此栏目，请添加后修改！');
+                }
+            }else{
+                return $this->error($validate->getError());
+            }
+        }else{
+            return $this->error('请求错误，请使用正确请求方式！');
+        }
+
+    }
+
+    public function del($id){
+        $did = Db('cate')->where('id',$id)->find();
+        if($did){
+            \db('cate')->where('id',$id)->delete();
+            return $this->success('删除成功！！');
+        }else{
+            return $this->error('未找到该栏目！！');
+        }
+    }
+
 }
