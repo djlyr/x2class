909daad2e19523b8740e86813c2fab20164c0961
mrliu <9325186@qq.com>
2018-12-30 03:51:04 +0800

    享信课堂程序，用于职场培训课程 thinkphp5。1 php 7.0版本

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -12,11 +12,20 @@ use app\Admin\validate\Cate as Cvalidate;
 
 class Cate extends Controller
 {
+    protected $beforeActionList = [
+        'dels'  =>  ['only'=>'del'],
+    ];
+
+
+
+
+
     //载入视图文件
     public function lst(){
-        $lst = Dcate::paginate(10);
-        $page = $lst->render();
-        $this->assign('page',$page);
+        $clst  = new Dcate();
+        $lst = $clst->catetree();
+//        $page = $lst->render();
+//        $this->assign('page',$page);
         $this->assign('lst',$lst);
         return view();
     }
@@ -31,7 +40,7 @@ class Cate extends Controller
                 //查找栏目是否存在
                 $cateto = Dcate::where('catename',$post['catename'])->find();
                 if(!$cateto){
-                    Dcate::data($post)->insert();
+                    Dcate::insert($post);
                     return $this->success('添加栏目成功！！','cate/lst');
                 }else{
                     return $this->error('栏目已经存在请更换栏目名称！！！');
@@ -42,7 +51,8 @@ class Cate extends Controller
             }
 
         }else{
-            $lst = Dcate::select();
+            $clst  = new Dcate();
+            $lst = $clst->catetree();
             return view()->assign('lst',$lst);
         }
 
@@ -62,16 +72,12 @@ class Cate extends Controller
         $did = Dcate::find($id);
         if($did){
             //能查询到数据
-
-            $qt['qid'] = Dcate::select();
-            $eid=array_merge($did,$qt);
-            if(!$eid){
-                return $this->error('提交数据错误，请返回重新提交！');
-            }else{
-                return view()->assign('eid',$eid);
-            }
-
-        }else{
+            $qt1 = new Dcate();
+            $qt = $qt1->catetree();
+            $this->assign('qt',$qt);
+            $this->assign('eid',$did);
+                return view();
+            } else{
             //不能查询到数据
             return $this->error('未找到编辑栏目，请重新提交查询！！！');
         }
@@ -108,6 +114,15 @@ class Cate extends Controller
             return $this->success('删除成功！！');
         }else{
             return $this->error('未找到该栏目！！');
+        }
+    }
+
+    public function dels(){
+        $cateid = input('id');
+        $catem = new Dcate();
+        $delid=$catem->todels($cateid);
+        if($delid){
+            $catem->delete($delid);
         }
     }
 
--- application/admin/model/Cate.php
+++ application/admin/model/Cate.php
@@ -7,4 +7,39 @@ use think\Model;
 class Cate extends Model
 {
     //
+
+    public function catetree(){
+        $lst = $this->select();
+        return $this->sort($lst);
+
+    }
+
+    public function sort($date,$pid=0,$level=0)
+    {
+        static $arr=array();
+        foreach ($date as $k => $v){
+            if($v['pid'] == $pid){
+                $v['level']=$level;
+                $arr[]=$v;
+                $this->sort($date,$v['id'],$level+1);
+            }
+        }
+        return $arr;
+    }
+
+    public function todels($cateid){
+        $cateall = $this->select();
+        return $this->allpid($cateall,$cateid);
+    }
+
+    public function allpid($cateall,$cateid){
+        static $arr = array();
+        foreach ($cateall as $k => $v){
+            if($v['pid'] == $cateid){
+                $arr[]=$v['id'];
+                $this->allpid($cateall,$v['id']);
+            }
+        }
+        return $arr;
+    }
 }
